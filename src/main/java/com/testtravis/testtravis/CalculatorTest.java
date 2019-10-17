9c3ae687d7fcdbfa617285a90a3ac289c7658660
gansing32 <gansing32@gmail.com>
2019-10-17 16:05:10 +0900

    ㅎㅎ

--- src/main/java/com/testtravis/testtravis/CalculatorTest.java
+++ src/main/java/com/testtravis/testtravis/CalculatorTest.java
@@ -1,12 +1,17 @@
 package com.testtravis.testtravis;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class CalculatorTest {
-    public void returnCalcul(){
+    @GetMapping("/")
+    public long returnCalcul(){
         long num1=1;
         long num2=2;
         long sum=Calculator.sum(num1,num2);
         if (sum!=3) throw new AssertionError();
+        return sum;
     }
 }
--- src/main/java/com/testtravis/testtravis/TesttravisApplication.java
+++ src/main/java/com/testtravis/testtravis/TesttravisApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TesttravisApplication {
-
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.yml,"
+            + "/app/config/springboot-webservice/real-application.yml";
     public static void main(String[] args) {
         SpringApplication.run(TesttravisApplication.class, args);
     }
