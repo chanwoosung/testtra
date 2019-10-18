83d3f322367c65955eb31a6075f14269541e2982
gansing32 <gansing32@gmail.com>
2019-10-18 12:43:23 +0900

    변경

--- src/main/java/com/testtravis/testtravis/CalculatorTest.java
+++ src/main/java/com/testtravis/testtravis/CalculatorTest.java
@@ -8,10 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class CalculatorTest {
     @GetMapping("/")
     public long returnCalcul(){
-        long num1=1;
-        long num2=2;
+        long num1=112341;
+        long num2=218561;
         long sum=Calculator.sum(num1,num2);
-        if (sum!=3) throw new AssertionError();
         return sum;
     }
 }
