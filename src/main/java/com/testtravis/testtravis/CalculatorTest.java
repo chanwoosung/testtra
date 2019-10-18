12ebe2fed780dfed96bd5b8fa0e2447461657a01
gansing32 <gansing32@gmail.com>
2019-10-18 16:45:43 +0900

    autowire 추가

--- src/main/java/com/testtravis/testtravis/CalculatorTest.java
+++ src/main/java/com/testtravis/testtravis/CalculatorTest.java
@@ -2,6 +2,7 @@ package com.testtravis.testtravis;
 
 
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.Arrays;
 @RestController
 @AllArgsConstructor
 public class CalculatorTest {
+    @Autowired
     private Environment env;
 
     @GetMapping("/")
